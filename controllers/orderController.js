9d28def9042755cdce318c018658312732c1f428
balakrishnan4591 <balakrishnan4591@gmail.com>
2024-10-24 19:21:06 +0530

    updated frontend url

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
   // const frontend_url = "http://localhost:5173";
-  const frontend_url = "https://cleanease-user.netlify.app/";
+  const frontend_url = "https://cleanease-user.netlify.app";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
